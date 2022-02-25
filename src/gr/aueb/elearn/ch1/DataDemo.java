4a6493ce3b0b6563797f64e3c545bdd8573a78f7
panvaggalis <panvaggalis@gmail.com>
2022-02-25 11:22:06 +0200

    Add Demi v.8.1

---
+++ src/gr/aueb/elearn/ch1/DataDemo.java
@@ -0,0 +1,16 @@
+package gr.aueb.elearn.ch1;
+
+public class DataDemo {
+
+    public static void main(String[] args) {
+
+        int num1 = 12;
+        int num2 = 5;
+        int sum;
+
+        sum = num1+num2;
+
+        System.out.println("Το άθροισμα των 12 και 5 είναι " + sum);
+
+    }
+}
